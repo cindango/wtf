f6bc3988b51be5eae41f22d4471bf0372c2b8e5a
Cindy Ho <cindango@gmail.com>
2013-12-27 17:18:53 -0500

    added signup and users

--- Gemfile
+++ Gemfile
@@ -8,6 +8,7 @@ gem 'jquery-rails'
 gem 'turbolinks'
 gem 'jbuilder', '~> 1.2'
 gem 'bootstrap-sass'
+gem 'devise', '~> 3.1.1' 
 
 group :doc do
   gem 'sdoc', require: false
--- config/environments/production.rb
+++ config/environments/production.rb
@@ -77,4 +77,7 @@ Wtf::Application.configure do
 
   # Use default logging formatter so that PID and timestamp are not suppressed.
   config.log_formatter = ::Logger::Formatter.new
+  
+  # required for heroku
+  config.action_mailer.default_url_options = { :host => 'http://grape-vine.herokuapp.com/' }
 end
