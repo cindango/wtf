663d54a0b99170a77a63adac32ffc2a34dd8bc06
Cindy Ho <cindango@gmail.com>
2013-12-27 17:04:34 -0500

    heroku stuff

---
+++ vendor/bundle/ruby/2.0.0/gems/atomic-1.1.14/ext/siteconf20131227-97597-hztjnb.rb
@@ -0,0 +1,6 @@
+require 'rbconfig'
+dest_path = "./.gem.20131227-97597-12h47k9"
+RbConfig::MAKEFILE_CONFIG['sitearchdir'] = dest_path
+RbConfig::CONFIG['sitearchdir'] = dest_path
+RbConfig::MAKEFILE_CONFIG['sitelibdir'] = dest_path
+RbConfig::CONFIG['sitelibdir'] = dest_path
---
+++ vendor/bundle/ruby/2.0.0/gems/json-1.8.1/ext/json/ext/generator/siteconf20131227-97597-164p0bt.rb
@@ -0,0 +1,6 @@
+require 'rbconfig'
+dest_path = "./.gem.20131227-97597-yyxp9a"
+RbConfig::MAKEFILE_CONFIG['sitearchdir'] = dest_path
+RbConfig::CONFIG['sitearchdir'] = dest_path
+RbConfig::MAKEFILE_CONFIG['sitelibdir'] = dest_path
+RbConfig::CONFIG['sitelibdir'] = dest_path
